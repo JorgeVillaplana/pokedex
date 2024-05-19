891642d23252c4399321414768ab8900b740323f
JorgeVillaplana <jorge.villaplana.m@gmail.com>
2024-05-19 14:39:11 +0200

    API 0.02

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@ const APIUrl = "https://pokeapi.co/api/v2/";
 const fetchPokemon = async (pokemon) => {
 
     const APIResponse = await fetch(`${APIUrl}${pokemon}`);
-    console.log(APIResponse);
+    const data = await APIResponse.json();
+    return data;
 }
 
+const renderPokemon = async (pokemon) => {
+
+    const data = await fetchPokemon(pokemon);
+    
+}
